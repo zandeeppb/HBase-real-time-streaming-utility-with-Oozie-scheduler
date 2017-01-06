79d388b960db816c512ecf96d2f3683b55eb9221
zandeeppb <zandeeppb@gmail.com>
2017-01-06 15:20:20 +0530

    Create Constants.java

---
+++ src/main/java/com/hbase/read/constant/Constants.java
@@ -0,0 +1,29 @@
+package com.hbase.read.constant;
+
+public class Constants {
+	public static final String tablename = "HBASE_TABLE_TO_READ";	
+	public static final String track_tablename = "HBASE_TRACK_TABLE";	
+	public static final String EMPTY_STRING = "";	
+	public static final String ATTRIBUTES = "ATTRIBUTES";
+	public static final String STATUS ="STATUS";
+	public static final String IN_PROGRESS  ="IN_PROGRESS";
+	public static final String INPROGRESS  ="IN PROGRESS";	
+	public static final String COMPLETED = "COMPLETED";
+	public static final String ERROR=  "ERROR";
+	public static final String PROJECT_ID=  "PROJECT_ID";
+	public static final String CUSTOMER_ID= "CUSTOMER_ID";	
+	public static final String SUCC_LAST_REC_TIMESTAMP ="SUCC_LAST_REC_TIMESTAMP";
+	public static final String ERR_LAST_REC_TIMESTAMP= "ERR_LAST_REC_TIMESTAMP";	
+	public static final String TRACK_TABLE_ROWKEY= "TRACK_TABLE_ROWKEY";	
+	public static final String ROW_KEY= "ROW_KEY";	
+	public static final String SUCCEEDED = "SUCCEEDED";	
+	public static final String UTF_8= "UTF-8";
+	public static final String SJS_STATUS = "status";	
+	public static final String ZERO= "0";	
+	public static final String FAILURE = "FAILURE";
+	public static final String CONTENT_TYPE = "Content-type";
+	public static final String CONTENT_TYPE_VALUE_ENCODED =  "application/x-www-form-urlencoded";
+	public static final String CONTENT_TYPE_VALUE_JSON = "application/json";	
+	public static final String UNDER_SCORE = "_";	
+	public static final String HBAE_QUORUM = "hbase.zookeeper.quorum";	
+}
